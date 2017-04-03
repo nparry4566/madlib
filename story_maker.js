529e002f09cbc7ba9e92a5df2e2836ee870a2ed1
nparry4566 <nparry4566@greenvilleschools.us>
2017-04-03 09:33:28 -0400

    Add files via upload

---
+++ story_maker.js
@@ -0,0 +1,35 @@
+function makeStory() {
+    // get form values (2 points)
+    var noun1 = document.getElementById('noun1').value;
+    var noun2 = document.getElementById('noun2').value;
+    var noun3 = document.getElementById('noun3').value;
+    var noun4 = document.getElementById('noun4').value;
+    var noun5 = document.getElementById('noun5').value;
+    var noun6 = document.getElementById('noun6').value;
+    var male_name1 = document.getElementById('male_name1').value;
+    var female_name1 = document.getElementById('female_name1').value;
+    var plural_noun1 = document.getElementById('plural_noun1').value;
+    var plural_noun2 = document.getElementById('plural_noun2').value;
+    var adjective1 = document.getElementById('adjective1').value;
+    var adjective2 = document.getElementById('adjective2').value;
+    var adjective3 = document.getElementById('adjective3').value;
+    var adjective4 = document.getElementById('adjective4').value;
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    var adverb1 = document.getElementById('adverb1').value;
+    var body_part1 = document.getElementById('body_part1').value;
+
+
+    // Set title of story. Use at least one form value in the title. (2 points)
+    var title = "Vacations";
+
+
+    // Build story. you can add as many paragraphs as you like. (3 points)
+    var paragraph1 = "Friends, this is a surprise party for" + Female;
+
+
+
+    // Display story by putting title & paragraphs into appropriate divs. (3 points)
+
+
+}
